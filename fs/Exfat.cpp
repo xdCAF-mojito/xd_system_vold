b4df70d73fbc01566dbffca291132bcf5858186c
Park Ju Hyung <qkrwngud825@gmail.com>
2019-03-26 01:46:57 +0900

    exfat: allow usage even without userspace tools
    Change-Id: I41b46475511ae5e978ae871942ce441bab95b6b2
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- fs/Exfat.cpp
+++ fs/Exfat.cpp
@@ -34,11 +34,15 @@ static const char* kMkfsPath = "/system/bin/mkfs.exfat";
 static const char* kFsckPath = "/system/bin/fsck.exfat";
 
 bool IsSupported() {
-    return access(kMkfsPath, X_OK) == 0 && access(kFsckPath, X_OK) == 0 &&
-           IsFilesystemSupported("exfat");
+    return IsFilesystemSupported("exfat");
 }
 
 status_t Check(const std::string& source) {
+    if (access(kFsckPath, X_OK)) {
+        LOG(WARNING) << "Missing fsck.exfat, skipping check...";
+        return 0;
+    }
+
     std::vector<std::string> cmd;
     cmd.push_back(kFsckPath);
     cmd.push_back(source);
